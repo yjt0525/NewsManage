50ede373f62e6854b4f042ba5cc149329fca8e96
佳涛 应 <yjt15851481743@outlook.com>
2017-09-19 10:52:32 +0800

    123

--- src/components/Manage/NewsList.vue
+++ src/components/Manage/NewsList.vue
@@ -11,108 +11,17 @@
             </el-form>
         </div>
         <div class='card-list flex'>
-            <el-card class="box-card">
+            <el-card v-for="(item,index) in newslist" :key="item.id" class="box-card">
                 <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
+                    <span style="line-height: 36px;">{{ item.newstitle }}</span>
                 </div>
                 <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
+                    {{ item.newscontent }}
                 </div>
                 <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
-                </div>
-            </el-card>
-            <el-card class="box-card">
-                <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
-                </div>
-                <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
-                </div>
-                <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
-                </div>
-            </el-card>
-            <el-card class="box-card">
-                <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
-                </div>
-                <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
-                </div>
-                <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
-                </div>
-            </el-card>
-            <el-card class="box-card">
-                <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
-                </div>
-                <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
-                </div>
-                <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
-                </div>
-            </el-card>
-            <el-card class="box-card">
-                <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
-                </div>
-                <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
-                </div>
-                <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
-                </div>
-            </el-card>
-            <el-card class="box-card">
-                <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
-                </div>
-                <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
-                </div>
-                <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
-                </div>
-            </el-card>
-            <el-card class="box-card">
-                <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
-                </div>
-                <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
-                </div>
-                <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
-                </div>
-            </el-card>
-            <el-card class="box-card">
-                <div slot="header" class="clearfix news-title">
-                    <span style="line-height: 36px;">这是标题</span>
-                </div>
-                <div class="news-content">
-                    这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容
-                </div>
-                <div class="news-otherinfo">
-                    <div class="news-type">青春</div> 
-                    <div class="publish-time">2017-09-09 14:50:00</div>
-                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove"></el-button>
+                    <div class="news-type">{{ item.newstype }}</div> 
+                    <div class="publish-time">{{ item.publishtime }}</div>
+                    <el-button type="danger" class="remove-news" icon="delete" @click="bindremove(index,item)"></el-button>
                 </div>
             </el-card>
         </div>
@@ -137,7 +46,56 @@ export default {
     data () {
         return {
             keyword:'',
-            pageIndex:1
+            pageIndex:1,
+            newslist:[{
+                id:0,
+                newstitle:'这是标题',
+                newscontent:'1的内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            },{
+                id:1,                
+                newstitle:'这是标题',
+                newscontent:'2的内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            },{
+                id:2,                
+                newstitle:'这是标题',
+                newscontent:'这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            },{
+                id:3,                
+                newstitle:'这是标题',
+                newscontent:'这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            },{
+                id:4,                
+                newstitle:'这是标题',
+                newscontent:'这是内容这是内容这是内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            },{
+                id:5,                
+                newstitle:'这是标题',
+                newscontent:'这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            },{
+                id:6,                
+                newstitle:'这是标题',
+                newscontent:'这是内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            },{
+                id:7,                
+                newstitle:'这是标题',
+                newscontent:'这是内容这是内容这是内容这是内容',
+                newstype:'青春',
+                publishtime:'2017-09-09 17:17:00'
+            }]
         }
     },
     methods:{
@@ -147,13 +105,14 @@ export default {
         addnews:function(){
             console.log('新建了');
         },
-        bindremove:function(){
+        bindremove:function(index,item){
             this.$confirm('您确定删除此条新闻吗', '警告', {
                 confirmButtonText: '确定',
                 cancelButtonText: '取消',
                 type: 'warning'
             }).then(() => {
-                 this.$message.success('删除成功');
+                this.newslist.splice(index,1);
+                this.$message.success('删除成功');
             }).catch(function(){
 
             })
