38116080a75db61285bad1d28160f499f3b2c504
xuyc <xuyc@think-land.com>
2017-10-09 14:54:34 +0800

    axios封装

--- src/components/view/Manage.vue
+++ src/components/view/Manage.vue
@@ -11,6 +11,7 @@
 </template>
 
 <script>
+import http from '../../http.js'
 import topbar from '../common/topbar'
 import sidenav from '../common/sidenav'
 export default {
@@ -23,15 +24,19 @@ export default {
             
         }
     },
+     mounted:function(){
+        this.getList()
+    },
     methods:{
-        
+        //async 表示一个async函数 await只能在这个函数中使用
+        getList:async function(){
+            let params={}
+            //await 请求是异步的 此处await表示当请求结束后再执行下一句语句 将异步转变为同步 ----await 后最好跟着一个异步操作或者一个耗时操作
+            const res = await http.get('/topic/5433d5e4e737cbe96dcef312',params)
+            console.log(res) 
+        }
     },
-    created:function(){
-
-    },
-    mounted:function(){
-        
-    }
+   
 }
 </script>
 
